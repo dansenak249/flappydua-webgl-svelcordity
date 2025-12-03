3f5f6d2ef3d6eecc2d1784becfe8b76c53204b80
Duy Anh Nguyen <63808308+dansenak249@users.noreply.github.com>
2025-12-03 09:49:08 +0700

    troi oii

--- src/lib/server/db.js
+++ src/lib/server/db.js
@@ -3,12 +3,13 @@ import { MongoClient } from "mongodb";
 import { env } from "$env/dynamic/private";
 
 let client;
+let db;
 
-export async function getDb() {
+export async function connectDB() {
     if (!client) {
         client = new MongoClient(env.MONGODB_URI);
         await client.connect();
+        db = client.db("1minute-flappydua");
     }
-
-    return client.db();
+    return db;
 }
