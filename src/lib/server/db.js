256cca56f55709999c9860ef4a57bfb73466f06b
Duy Anh Nguyen <63808308+dansenak249@users.noreply.github.com>
2025-12-10 16:08:52 +0700

    database

--- src/lib/server/db.js
+++ src/lib/server/db.js
@@ -1,5 +1,5 @@
 // src/lib/server/db.js
-import { MongoClient } from "mongodb";
+import { MongoClient, ServerApiVersion } from "mongodb";
 import { env } from "$env/dynamic/private";
 
 let client;
@@ -7,9 +7,20 @@ let db;
 
 export async function connectDB() {
     if (!client) {
-        client = new MongoClient(env.MONGODB_URI);
+        client = new MongoClient(env.MONGODB_URI, {
+            serverApi: {
+                version: ServerApiVersion.v1,
+                strict: true,
+                deprecationErrors: true,
+            },
+        });
+
         await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("MongoDB connected successfully!");
+
         db = client.db("1minute-flappydua");
     }
+
     return db;
 }
